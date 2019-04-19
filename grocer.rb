21482fe3e03a722ae4c22ad6404d87e55e24eeef
Blake A. Long <blake.a.long@gmail.com>
2019-04-19 11:46:20 -0600

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,59 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each do | hash |
+    hash.each do | item, stats |
+    if consolidated[item]
+      consolidated[item][:count] += 1  
+    else
+      consolidated[item] = stats
+      consolidated[item][:count] = 1
+    end
+   end
+  end
+ consolidated
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+ new_cart = {}
+ cart.each do | food, hash |
+  coupons.each do | coupon |
+    if food == coupon[:item] && hash[:count] >= coupon[:num]
+      hash[:count] -= coupon[:num]
+      if new_cart["#{food} W/COUPON"]
+        new_cart["#{food} W/COUPON"][:count] += 1 
+      else 
+        new_cart["#{food} W/COUPON"] = 
+        { price: coupon[:cost], clearance: hash[:clearance], count: 1 }
+      end
+     end
+    end
+   new_cart[food] = hash
+  end
+ new_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  clearance_cart = cart
+  cart.each do | food, hash |
+    if hash[:clearance] 
+      clearance_cart[food][:price] = (hash[:price] * 0.8).round(2) 
+     else 
+      clearance_cart[food][:price] = hash[:price]
+    end
+  end
+ clearance_cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cons_cart = consolidate_cart(cart)
+  coup_cart = apply_coupons(cons_cart, coupons)
+  clear_cart = apply_clearance(coup_cart)
+
+  cart_total = 0
+  clear_cart.each do | food, hash |
+  cart_total += (hash[:price] * hash[:count])
+  end
+  cart_total > 100 ? cart_total * 0.9 : cart_total
 end
